31cfbf6c6789dde7e1c187cf154154edccbe8923
[priya111260] <[reshma.p@cognateglobal.com]>
2021-06-24 12:54:47 +0530

    Create cd_tems.py

---
+++ cd_tems.py
@@ -0,0 +1,11 @@
+tea=20
+if tea<10:
+    print('i drink tea')
+elif tea>20 and tea>=10:
+    print('i will buy the tea')
+elif tea==20:
+        print('i want money')
+else:
+            print('i not money')
+                
+            
